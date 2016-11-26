e018378c3f746a0e1a1f061850075a8e9a6d6778
Alejandro H Tadino M <alejandrohtadinom@gmail.com>
2016-11-26 01:31:03 -0400

    Added: page title and empty array

--- js/app.js
+++ js/app.js
@@ -13,7 +13,9 @@
 		$scope.checkList = function(luncList) {
 			var str = luncList.split(',');
 
-			if (str.length > 3) {
+			if (str.length == 0) {
+				$scope.message = "Please enter data first";
+			} else if (str.length > 3) {
 				$scope.message = "Too much!";
 			} else {
 				$scope.message = "Enjoy!";
