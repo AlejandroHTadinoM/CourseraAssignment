1a6c372e8c4952e312e5ccdc492d46b657068e87
Alejandro H Tadino M <alejandrohtadinom@gmail.com>
2016-11-26 01:12:25 -0400

    First commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+(function () {
+	'use strict';
+
+	angular.module('LunchCheck', [])
+	.controller('LunchCheckController', LunchCheckController);
+
+	LunchCheckController.$injector = ['$scope'];
+
+	function LunchCheckController($scope) {
+		$scope.luncList = [];
+		$scope.message = '';
+
+		$scope.checkList = function(luncList) {
+			var str = luncList.split(',');
+
+			if (str.length > 3) {
+				$scope.message = "Too much!";
+			} else {
+				$scope.message = "Enjoy!";
+			};
+		};
+	}
+})();
